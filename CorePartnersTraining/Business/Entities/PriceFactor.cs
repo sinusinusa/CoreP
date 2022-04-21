b5e82a41dbdf6e2c9e33759e8477d98f5c743435
Nikita Lysikov <dr.orange48@gmail.com>
2022-04-21 20:22:03 +0300

    smth for conflict

--- CorePartnersTraining/Business/Entities/PriceFactor.cs
+++ CorePartnersTraining/Business/Entities/PriceFactor.cs
@@ -13,11 +13,12 @@ namespace CorePartnersTraining.Business.Entities
 
     PriceFactor()
     {
-      Console.WriteLine("again");
+      Console.WriteLine("wat");
     }
     public float Omega()
     {
-      return 0.05F;
+      return GetV == null ? 0 : GetV.Value;
+      return 1F;
     }
   }
 
